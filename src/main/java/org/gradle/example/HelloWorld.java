a9f7373c69fc7b7c3fdcfafcbaa40d7a45832d9a
tm1c14 <tm1c14@soton.ac.uk>
2015-06-17 17:03:08 +0100

    init app

---
+++ src/main/java/org/gradle/example/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.gradle.example;
+
+/**
+ * Created by tm1c14 on 17/06/2015.
+ */
+public class HelloWorld {
+    public static void main(String ...args) {
+        System.out.println("Hello World..");
+    }
+}
