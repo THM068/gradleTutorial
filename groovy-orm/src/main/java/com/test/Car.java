b7e5172202455d179ac0bf6974bb208ec849ef41
tm1c14 <tm1c14@soton.ac.uk>
2015-06-18 17:06:58 +0100

    started multiple project build

---
+++ groovy-orm/src/main/java/com/test/Car.java
@@ -0,0 +1,11 @@
+package com.test;
+
+/**
+ * Created by tm1c14 on 18/06/2015.
+ */
+public class Car {
+
+    public void drive() {
+        System.out.println("Drive me to mexico");
+    }
+}
---
+++ srcAdditional/main/java/com/additional/Person.java
@@ -0,0 +1,11 @@
+package main.java.com.additional;
+
+/**
+ * Created by tm1c14 on 18/06/2015.
+ */
+public class Person {
+
+    static {
+        System.out.println("I am a person ....");
+    }
+}
